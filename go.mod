eb4fd1be3c9b1a61e86cdef11606eabdfbd0c627
nich.otieno <nich.otieno@gmail.com>
2025-08-11 13:10:08 +0300

    Add godotenv package with initial implementation and documentation
    - Added .gitignore for godotenv vendor directory.
    - Included LICENSE file for godotenv under MIT License.
    - Added README.md with installation instructions, usage examples, and contribution guidelines for godotenv.
    - Implemented core functionality in godotenv.go, including Load, Overload, Read, and Write methods for environment variable management.
    - Created parser.go for parsing .env files with support for comments, quoted values, and variable expansion.
    - Updated modules.txt to include godotenv as a dependency.

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/crucialjun/rss_aggregator
 
 go 1.22.2
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/joho/godotenv v1.5.1
+
+require github.com/go-chi/chi/v5 v5.2.2 // indirect
--- main.go
+++ main.go
@@ -3,19 +3,39 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	godotenv.Load()
 
 	portString := os.Getenv("PORT")
 
 	if portString == "" {
 		log.Fatal(
 			"PORT environment variable is not set",
-		) 
+		)
 	}
 
-	fmt.Println("Server is running on port:", portString)
+	router := chi.NewRouter()
+
+	server := &http.Server{
+		Addr:    ":" + portString,
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+
+	log.Println("Starting server on port:", portString)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
